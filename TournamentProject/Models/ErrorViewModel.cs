5ff9f73970c3d79b32f028a281f6ece3f3dbdafd
Sobh6n <sobhankhedry7297@gmail.com>
2024-10-27 18:25:42 +0330

    added everything

---
+++ TournamentProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TournamentProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
