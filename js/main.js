a6e686bba927952dc39907512244d35ebc064d64
nazanin <nazaninniazi144@gmail.com>
2024-10-27 07:54:04 -0700

    all the files added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if(position >= 200 ){
+        $('.nav-menu').addClass('custom-navbar')
+    }else{
+        $('.nav-menu').removeClass('custom-navbar')
+    }
+})
