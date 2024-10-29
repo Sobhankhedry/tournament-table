475fb4c87b42ba209ecf22b805559dff3615d393
Sobh6n <sobhankhedry7297@gmail.com>
2024-10-29 11:38:08 +0330

    updated

---
+++ TournamentProject/Areas/Identity/Data/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TournamentProject.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the User class
+public class User : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
