9856b9e58e30f26ea1248f95bf3c35d47cd38150
Sobh6n <sobhankhedry7297@gmail.com>
2024-10-30 18:46:29 +0330

    deleted js

--- TournamentProject/wwwroot/js/main.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(){
-    let position = $(this).scrollTop();
-    if(position >= 200 ){
-        $('.nav-menu').addClass('custom-navbar')
-    }else{
-        $('.nav-menu').removeClass('custom-navbar')
-    }
-}) 
