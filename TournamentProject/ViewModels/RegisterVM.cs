10668163edd2e32d8ba5f727ef1b57f8b172832d
Sobh6n <sobhankhedry7297@gmail.com>
2024-11-01 10:45:34 +0330

    validation aligned right

--- TournamentProject/ViewModels/RegisterVM.cs
+++ TournamentProject/ViewModels/RegisterVM.cs
@@ -4,20 +4,20 @@ namespace TournamentProject.ViewModels
 {
     public class RegisterVM
     {
-        [Required(ErrorMessage = "نام و نام خانوادگی الزامی است.")]
+        [Required(ErrorMessage = "نام و نام خانوادگی الزامی است")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "ایمیل الزامی است.")]
-        [EmailAddress(ErrorMessage = "ایمیل وارد شده صحیح نیست.")]
+        [Required(ErrorMessage = "ایمیل الزامی است")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده صحیح نیست ")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "رمز عبور الزامی است.")]
+        [Required(ErrorMessage = "رمز عبور الزامی است")]
         [DataType(DataType.Password)]
         public string? Password { get; set; }
 
-        [Required(ErrorMessage = "تکرار رمز عبور الزامی است.")]
+        [Required(ErrorMessage = "تکرار رمز عبور الزامی است")]
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "رمز عبور و تکرار آن باید برابر باشد.")]
+        [Compare("Password", ErrorMessage = "رمز عبور و تکرار آن باید برابر باشد")]
         public string? ConfirmPassword { get; set; }
     }
 }
