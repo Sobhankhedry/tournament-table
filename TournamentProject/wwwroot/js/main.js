2e9ee0421fce61e155ef9d1012e121bb33201b86
Sobh6n <sobhankhedry7297@gmail.com>
2024-11-19 09:17:59 +0330

    scroll fixed

--- TournamentProject/wwwroot/js/main.js
+++ TournamentProject/wwwroot/js/main.js
@@ -13,3 +13,12 @@ $(document).ready(function () {
         $('.input-validation-error').css('border-color', 'red');
     });
 });
+
+//$(window).scroll(function () {
+//    let position = $(this).scrollTop();
+//    if (position >= 200) {
+//        $('.nav-menu').addClass('custom-navbar')
+//    } else {
+//        $('.nav-menu').removeClass('custom-navbar')
+//    }
+//}) 
